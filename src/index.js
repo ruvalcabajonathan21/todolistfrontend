8cebfed7ce18fd718b5b1e838bcb439020d90558
ruvalcabajonathan21 <ruvalcabajonathan21@gmail.com>
2019-05-23 09:38:42 -0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,94 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+
+import TodoItem from "./todo-item";
+import "./styles.css";
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      todo: "",
+      todos: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:5000/todos")
+    .then(response => response.json())
+    .then(data => {
+      this.setState({
+        todos: data
+      })
+    });
+  }
+
+  onChange = event => {
+    this.setState({
+      todo: event.target.value
+    });
+  };
+
+  renderTodos = () => {
+    return this.state.todos.map(item => {
+      return (<TodoItem key={item.id} item={item} deleteItem={this.deleteItem} />);
+    });
+  };
+
+  addTodo = event => {
+    event.preventDefault();
+    axios({
+      method: "post",
+      url: "http://localhost:5000/add-todo",
+      headers: { "content-type": "application/json"},
+      data: {
+        title: this.state.todo,
+        done: false
+      }
+    })
+    .then(data => {
+      this.setState({
+        todos: [...this.state.todos, data.data],
+        todo: ""
+      })
+    })
+    .catch(error => console.log(error));
+  };
+
+  deleteItem = id => (
+    fetch(`http://localhost:5000/todo/${id}`, {
+      method: "DELETE"
+    })
+    .then(this.setState({
+      todos: this.state.todos.filter(item => {
+        return item.id !== id
+      })
+    })
+    )
+    .catch(error => console.log(error))
+  )
+
+  render() {
+    return (
+      <div className="app">
+        <h1>ToDo List</h1>
+        <form className="add-todo" onSubmit={this.addTodo}>
+          <input
+            type="text"
+            placeholder="Add ToDo"
+            onChange={this.onChange}
+            value={this.state.todo}
+          />
+          <button type="submit">Add</button>
+        </form>
+        {this.renderTodos()}
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+
---
+++ src/todo-item.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+class TodoItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: props.done
+    };
+  }
+
+  toggleDone = () => {
+    fetch(`http://localhost:5000/todo/${this.props.item.id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        title: this.props.item.title,
+        done: !this.state.done
+      })
+    }) 
+      .then(this.setState({
+        done: !this.state.done
+      })
+    )
+    .catch(error => console.log(error));
+  }
+
+  render() {
+    return (
+      <div className="todo-item">
+        <input
+          type="checkbox"
+          onClick={this.toggleDone}
+          defaultChecked={this.state.done}
+        />
+        <p className={this.state.done ? "done" : null}>{this.props.item.title}</p>
+        <button onClick={() => this.props.deleteItem(this.props.item.id)}>X</button>
+      </div>
+    );
+  }
+}
+
+export default TodoItem;
